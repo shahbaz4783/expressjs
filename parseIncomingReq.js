0975026a8d0b8c95f7fe176c6e5c69ce004c8a79
shahbaz4783 <shahbaz4783@gmail.com>
2023-11-09 22:33:29 +0530

    creating file for parsed data

--- parseIncomingReq.js
+++ parseIncomingReq.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 const port = 3000;
 const app = express();
+import fs from 'fs/promises';
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // we can also use express body parser, so we will not have to install bodyparser, like this:
@@ -19,6 +21,13 @@ app.use('/add-post', (req, res) => {
 
 app.use('/posts', (req, res) => {
 	console.log(req.body);
+    
+    // This will create a file named 'post.txt' and put request in it
+    fs.writeFile('post.txt', JSON.stringify(req.body));
+
+    // to get only entered data
+    fs.writeFile('post.txt', JSON.stringify(req.body.post));
+
 	res.redirect('/');
 });
 
